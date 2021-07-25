ca24ab336b88ab48bda36ee9200af00b452b901d
silpagui <silpagui.sa@gmail.com>
2021-07-25 16:51:41 +0200

    feat(Author): apply accesibility

--- src/components/Author/Author.component.js
+++ src/components/Author/Author.component.js
@@ -3,9 +3,17 @@ import "./Author.styles.css";
 
 export function Author({ quoteAuthor, quoteGenre, onClick }) {
   return (
-    <button className="author" onClick={onClick}>
-      <h2 className="author-name">{quoteAuthor}</h2>
-      <p className="genres">{quoteGenre}</p>
+    <button
+      className="author"
+      onClick={onClick}
+      aria-label={`Author: ${quoteAuthor}. Genre: ${quoteGenre}`}
+    >
+      <span aria-hidden="true" className="author-name">
+        {quoteAuthor}
+      </span>
+      <span aria-hidden="true" className="genres">
+        {quoteGenre}
+      </span>
     </button>
   );
 }
