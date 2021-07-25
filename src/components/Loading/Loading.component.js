aa5a1fe29edb903062d0386c4e0ba9de4335d914
silpagui <silpagui.sa@gmail.com>
2021-07-25 17:50:10 +0200

    feat(Loading): create Loading component

---
+++ src/components/Loading/Loading.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Loading.styles.css";
+
+export function Loading() {
+  return (
+    <span className="loader">
+      <span className="loader-deco">Loading...</span>
+    </span>
+  );
+}
